61a12a188da3976a65d3ec2e84407072c0a0c4cc
PaLucena <pablolucena01@gmail.com>
2024-12-16 12:14:44 +0100

    feat: ex07 completed

--- python00/ex07/sos.py
+++ python00/ex07/sos.py
@@ -2,15 +2,57 @@ import sys
 
 
 def main():
+    morse_code = {
+        'A': '.-',
+        'B': '-...',
+        'C': '-.-.',
+        'D': '-..',
+        'E': '.',
+        'F': '..-.',
+        'G': '--.',
+        'H': '....',
+        'I': '..',
+        'J': '.---',
+        'K': '-.-',
+        'L': '.-..',
+        'M': '--',
+        'N': '-.',
+        'O': '---',
+        'P': '.--.',
+        'Q': '--.-',
+        'R': '.-.',
+        'S': '...',
+        'T': '-',
+        'U': '..-',
+        'V': '...-',
+        'W': '.--',
+        'X': '-..-',
+        'Y': '-.--',
+        'Z': '--..',
+        '0': '-----',
+        '1': '.----',
+        '2': '..---',
+        '3': '...--',
+        '4': '....-',
+        '5': '.....',
+        '6': '-....',
+        '7': '--...',
+        '8': '---..',
+        '9': '----.',
+        ' ': '/'
+    }
+
     try:
         if len(sys.argv) != 2:
             raise AssertionError("the arguments are bad")
-        
+        str = sys.argv[1].upper()
+        for c in str:
+            if not (c.isalnum() or c == ' '):
+                raise AssertionError("the arguments are bad")
+        morseStr = ' '.join(morse_code[c] for c in str)
+        print(morseStr)
     except AssertionError as e:
         print(f"AssertionError: {e}")
-    str.isalnum()
-    if any(char not in allowed for char in str):
-        raise AssertionError("the arguments are bad")
 
 if __name__ == "__main__":
     main()
